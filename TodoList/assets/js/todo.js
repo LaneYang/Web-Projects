06e0f7e36fd3cbd407285e6dae99131e7fbe4d27
zilingyang <yang.zil@husky.neu.edu>
2019-01-31 07:15:11 -0800

    delete function

--- TodoList/assets/js/todo.js
+++ TodoList/assets/js/todo.js
@@ -1,6 +1,18 @@
+//add strike and bg color when click.
 $("li").click(function () {
-    // $(this).css("color","grey");
-    // $(this).css("text-decoration","line-through");
-    $(this).toggleClass("completed");}
+        // $(this).css("color","grey");
+        // $(this).css("text-decoration","line-through");
+        $(this).toggleClass("completed");
+    }
 );
+//click X to delete a "todo list".
 
+$("span").click(function (event) {
+    //remove the parent tag of this tag.
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    //Prevents the event from bubbling up the DOM tree,
+    //preventing any parent handlers from being notified of the event.
+    event.stopPropagation();
+});
